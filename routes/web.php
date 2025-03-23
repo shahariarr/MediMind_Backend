a098a3668df4e34fd4dee2ac559529107f109dc3
Md. Shahariar Rahman <99550336+shahariarr@users.noreply.github.com>
2025-03-23 16:02:54 +0600

    Refactor authentication views and middleware; add Customer model and related pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,67 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// User Web API Routes
+Route::post('/user-registration',[UserController::class,'UserRegistration']);
+Route::post('/user-login',[UserController::class,'UserLogin']);
+Route::get('/user-profile',[UserController::class,'UserProfile'])->middleware('auth:sanctum');
+Route::get('/logout',[UserController::class,'UserLogout'])->middleware('auth:sanctum');
+Route::post('/user-update',[UserController::class,'UpdateProfile'])->middleware('auth:sanctum');
+Route::post('/send-otp',[UserController::class,'SendOTPCode']);
+Route::post('/verify-otp',[UserController::class,'VerifyOTP']);
+Route::post('/reset-password',[UserController::class,'ResetPassword'])->middleware('auth:sanctum');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Customer Web API Routes
+Route::post("/create-customer",[CustomerController::class,'CustomerCreate'])->middleware('auth:sanctum');
+Route::get("/list-customer",[CustomerController::class,'CustomerList'])->middleware('auth:sanctum');
+Route::post("/delete-customer",[CustomerController::class,'CustomerDelete'])->middleware('auth:sanctum');
+Route::post("/update-customer",[CustomerController::class,'CustomerUpdate'])->middleware('auth:sanctum');
+Route::post("/customer-by-id",[CustomerController::class,'CustomerByID'])->middleware('auth:sanctum');
+
+
+
+
+// Page Routes
+Route::view('/','pages.home');
+Route::view('/userLogin','pages.auth.login-page')->name('login');
+Route::view('/userRegistration','pages.auth.registration-page');
+Route::view('/sendOtp','pages.auth.send-otp-page');
+Route::view('/verifyOtp','pages.auth.verify-otp-page');
+Route::view('/resetPassword','pages.auth.reset-pass-page');
+Route::view('/userProfile','pages.dashboard.profile-page');
+
+
+Route::view('/categoryPage','pages.dashboard.category-page');
+Route::view('/customerPage','pages.dashboard.customer-page');
+
+
